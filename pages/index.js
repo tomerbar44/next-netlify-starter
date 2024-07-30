3abc18657f332e5fe3280c9733993efc98589f99
Tomer Bar <48451037+tomerbar44@users.noreply.github.com>
2024-07-30 15:33:49 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to DY Iframe demo" />
-        <img src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=REPLACE_WITH_CAMPAIGN_VERSION&amp;tag_ids=1021610" alt="demo">
+        <img src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=REPLACE_WITH_CAMPAIGN_VERSION&amp;tag_ids=1021610" alt="demo"/>
       </main>
     </div>
   )
