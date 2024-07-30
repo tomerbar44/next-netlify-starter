c8d5d97fe435a23ebd75379300a7b227291f5d71
Tomer Bar <48451037+tomerbar44@users.noreply.github.com>
2024-07-30 16:56:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,10 @@ export default function Home() {
   style={{ textDecoration: 'none' }} 
   target="_blank" 
   title="Dynamic Content Block 1" 
-  href="https://breeze.serving.global/v1/email/dc/click/8789843/1021611/1021610?utm_source=dynamic%20yield&utm_medium=email&utm_campaign=[CAMPAIGN NAME]&cuid=7&cuid_type=email&locale=5&version=REPLACE_WITH_CAMPAIGN_VERSION&tag_ids=1021610"
+  href="https://breeze.serving.global/v1/email/dc/click/8789843/1021611/1021610?utm_source=dynamic%20yield&utm_medium=email&utm_campaign=[CAMPAIGN NAME]&cuid=7&cuid_type=email&locale=5&version=1&tag_ids=1021610"
 >
   <img 
-    src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=REPLACE_WITH_CAMPAIGN_VERSION&amp;tag_ids=1021610" 
+    src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=1&amp;tag_ids=1021610" 
     alt="demo"
   />
 </a>
