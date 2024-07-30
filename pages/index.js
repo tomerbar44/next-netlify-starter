716451965ae3e67ead558ef3225570d5b5849a3a
Tomer Bar <48451037+tomerbar44@users.noreply.github.com>
2024-07-30 16:38:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Welcome to DY Iframe demo" />
-        <img src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=REPLACE_WITH_CAMPAIGN_VERSION&amp;tag_ids=1021610" alt="demo"/>
+        <a style="text-decoration: none;" target="_blank" title="Dynamic Content Block 1" href="https://breeze.serving.global/v1/email/dc/click/8789843/1021611/1021610?utm_source=dynamic%20yield&utm_medium=email&utm_campaign=[CAMPAIGN NAME]&cuid=7&cuid_type=email&locale=5&version=REPLACE_WITH_CAMPAIGN_VERSION&tag_ids=1021610">
+<img src="https://breeze.serving.global/v1/email/dc/image/8789843/1021611/1021610?utm_source=dynamic%20yield&amp;utm_medium=email&amp;utm_campaign=[CAMPAIGN NAME]&amp;cuid=7&amp;cuid_type=email&amp;locale=5&amp;version=REPLACE_WITH_CAMPAIGN_VERSION&amp;tag_ids=1021610" alt="demo"/>
+</a>
       </main>
     </div>
   )
